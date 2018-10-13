12539c89f3cbf2f501a76e0600668c7909ac1af0
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-13 00:06:38 -0400

    Rewrote variable usage, convert 2.x py to 3.6.x

--- diceroll.py
+++ diceroll.py
@@ -2,23 +2,23 @@ from random import randint
 from time import sleep
 
 def get_user_guess():
-	guess = raw_input("What is your guess? ")
+	guess = input("What is your guess? ")
 	return int(guess)
 
 def roll_dice(number_of_sides):
     first_roll = randint(1, number_of_sides)
     second_roll = randint(1, number_of_sides)
     max_val = number_of_sides * 2
-    print ("Your maximum value is %d") % max_val
+    print ("Your maximum value is " + str(max_val))
     guess = get_user_guess()
     if guess > max_val:
         print ("Your guess is invalid.")
     else:
         print ("Rolling...")
         sleep(2)
-        print ("%d") % first_roll
+        print (first_roll)
         sleep(1)
-        print ("%d") % second_roll
+        print (second_roll)
         sleep(1)
         total_roll = first_roll + second_roll
         print("Result...")
@@ -28,4 +28,4 @@ def roll_dice(number_of_sides):
         else:
             print("Bad luck! You sadly lost :<")
 
-roll_dice(6)
+roll_dice(6) #Traditional die pieces
