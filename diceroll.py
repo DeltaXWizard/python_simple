079561443025580bf8af3f147e98cd710f83aa50
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-13 00:39:21 -0400

    Loop for repetition, include mini sub-menu

--- diceroll.py
+++ diceroll.py
@@ -1,6 +1,10 @@
 from random import randint 
 from time import sleep
 
+def on_ready(): # For looping sakes.
+    y = input("How big do you want your dice size to be? ")
+    roll_dice(int(y))
+
 def get_user_guess():
 	guess = input("What is your guess? ")
 	return int(guess)
@@ -13,12 +17,21 @@ def roll_dice(number_of_sides):
     guess = get_user_guess()
     if guess > max_val:
         print ("Your guess is invalid.")
+        redo = input("Will you like to play again? Type yes to retry and no to exit. ")
+        if redo == "yes":
+            return on_ready()
+        elif redo == "no":
+            print("Going offline...")
+            sleep(1)
+            return # experimental.
+        else:
+            print("Please either be yes or no.")
     else:
-        print ("Rolling...")
+        print ("Rolling 2 die...")
         sleep(2)
-        print (first_roll)
+        print (str(first_roll) + " is your first roll!")
         sleep(1)
-        print (second_roll)
+        print (str(second_roll) + " is your second roll!")
         sleep(1)
         total_roll = first_roll + second_roll
         print("Result...")
@@ -26,6 +39,17 @@ def roll_dice(number_of_sides):
         if total_roll == guess:
             print("Congratulations! You win!")
         else:
-            print("Bad luck! You sadly lost :<")
+            print("Bad luck! You sadly lost :<\n\nThanks for playing... try again?")
+            end = input("Will you like to play again? Type yes to retry and no to exit. ")
+            if end == "yes":
+                return on_ready()
+            elif end == "no":
+                print("Going offline...")
+                sleep(1)
+                return # experimental.
+            else:
+                print("Please either be yes or no.")
 
-roll_dice(6) #Traditional dice cubes. 
+
+y = input("How big do you want your dice size to be? ") #default execution
+roll_dice(int(y))
