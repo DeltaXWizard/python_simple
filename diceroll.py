f068798c5fb6f7224deb13acf74fbd5219330e93
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-12 22:42:11 -0400

    Initial creation

---
+++ diceroll.py
@@ -0,0 +1,31 @@
+from random import randint 
+from time import sleep
+
+def get_user_guess():
+	guess = raw_input("What is your guess? ")
+	return int(guess)
+
+def roll_dice(number_of_sides):
+    first_roll = randint(1, number_of_sides)
+    second_roll = randint(1, number_of_sides)
+    max_val = number_of_sides * 2
+    print ("Your maximum value is %d") % max_val
+    guess = get_user_guess()
+    if guess > max_val:
+        print ("Your guess is invalid.")
+    else:
+        print ("Rolling...")
+        sleep(2)
+        print ("%d") % first_roll
+        sleep(1)
+        print ("%d") % second_roll
+        sleep(1)
+        total_roll = first_roll + second_roll
+        print("Result...")
+        sleep(1)
+        if total_roll == guess:
+            print("Congratulations! You win!")
+        else:
+            print("Bad luck! You sadly lost :<")
+
+roll_dice(6)
