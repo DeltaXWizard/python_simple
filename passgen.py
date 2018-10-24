b933310e47d63fca49b91a8222ddbfa30d1317d7
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-23 21:18:52 -0400

    Include menu, retry, and optimize file

--- passgen.py
+++ passgen.py
@@ -1,9 +1,11 @@
 ### A simple password generator.
 
 import random
+import itertools
 
+from time import sleep
 
-def on_ready():  # For looping sakes.
+def on_ready():
     length = input("How long do you want your password to be?")
     length = int(length)
     return selection(length)
@@ -15,14 +17,28 @@ def selection(length):
     return main(length, s)
 
 
-def main(length, s):  # MUST execute first
+def main(length, s):
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_-+={[}]|\:;\"'<,>.?/"
+    password = ''
 
-    for p in range(s):
-        password = ''
+    for x in itertools.repeat(None, s):
         for c in range(length):
             password += random.choice(chars)
-        print(password)
 
+        print(password)
+        password = ''
+
+    return redo()
+
+def redo():
+    redo = input("Will you like to get more? Type yes to retry and no to exit. ")
+    if redo == "yes":
+        return on_ready()
+    elif redo == "no":
+        print("Going offline...")
+        sleep(1)
+        return
+    else:
+        print("Please either be yes or no.")
 
 on_ready()
