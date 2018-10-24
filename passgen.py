544159fcf992fc3c92b91bc15e9af83d5b5cca03
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-23 20:26:53 -0400

    Convert to loop

--- passgen.py
+++ passgen.py
@@ -2,16 +2,23 @@
 
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_-+={[}]|\:;\"'<,>.?/"
+def on_ready(): 
+    length = input("How long do you want your password to be?")
+    length = int(length)
+    return selection(length)
 
-length = input("How long do you want your password to be?")
-length = int(length)
+def selection(length):
+    suggestion = input("How many passwords would you like to choose from?")
+    s = int(suggestion)
+    return main(length, s)
 
-suggestion = input("How many passwords would you like to choose from?")
-s = int(suggestion)
+def main(length, s):
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_-+={[}]|\:;\"'<,>.?/"
 
-for p in range(s):
-  password = ''
-  for c in range(length):
-    password += random.choice(chars)
-  print(password)
+    for p in range(s):
+      password = ''
+      for c in range(length):
+        password += random.choice(chars)
+      print(password)
+
+on_ready()
