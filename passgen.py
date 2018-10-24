57536450d17e3efe5b1a0f4c75f43d81212ec794
DeltaX <33706469+DeltaXWizard@users.noreply.github.com>
2018-10-23 20:28:53 -0400

    Fix spacing error {organize}

--- passgen.py
+++ passgen.py
@@ -2,23 +2,27 @@
 
 import random
 
-def on_ready(): 
+
+def on_ready():  # For looping sakes.
     length = input("How long do you want your password to be?")
     length = int(length)
     return selection(length)
+
 
 def selection(length):
     suggestion = input("How many passwords would you like to choose from?")
     s = int(suggestion)
     return main(length, s)
 
-def main(length, s):
+
+def main(length, s):  # MUST execute first
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_-+={[}]|\:;\"'<,>.?/"
 
     for p in range(s):
-      password = ''
-      for c in range(length):
-        password += random.choice(chars)
-      print(password)
+        password = ''
+        for c in range(length):
+            password += random.choice(chars)
+        print(password)
+
 
 on_ready()
